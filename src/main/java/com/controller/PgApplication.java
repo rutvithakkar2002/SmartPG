4486159081f465e23925ebbc4fdb5898154a61a7
Rutvi Thakkar <thakkarrutvi2002@gmail.com>
2022-02-09 10:26:34 +0530

    init

---
+++ src/main/java/com/controller/PgApplication.java
@@ -0,0 +1,10 @@
+package com.controller;
+
+public class PgApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/controller/ServerInitializer.java
@@ -0,0 +1,9 @@
+package com.controller;
+
+public class ServerInitializer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+}
